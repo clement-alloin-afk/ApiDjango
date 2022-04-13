915378fb9bee7f61d36db53054441e480afc038f
Alloin Clement <clement.alloin00@gmail.com>
2022-04-13 18:22:06 +0200

    Modif Swagger & Url

--- DjangoAPI/DjangoAPI/urls.py
+++ DjangoAPI/DjangoAPI/urls.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_simplejwt import views as jwt_views
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="TFE API",
+      default_version='v1',
+   ),
+   public=True,
+)
 urlpatterns = [
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token-refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('myapp.urls')),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token-refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- DjangoAPI/myapp/urls.py
+++ DjangoAPI/myapp/urls.py
@@ -12,55 +12,44 @@ TacheList,TacheDetail,ListeTacheList
  )
 from . import views
 
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="TFE API",
-      default_version='v1',
-   ),
-   public=True,
-)
 
 urlpatterns = [
-    re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('Family/', FamilyList.as_view()),
-    path('Family/<int:pk>/', FamilyDetail.as_view()),
-    re_path(r'^Family/(?P<code>[A-Z0-9]{10})/$', views.FamilyCode),
-    path('Family/<int:pkF>/Membres/', FamilyMembresList.as_view()),
-    path('Family/<int:pkF>/Repas/', FamilyRepasList.as_view()),
-    path('Family/<int:pkF>/Repas/<pk>', FamilyRepasDetail.as_view()),
-    path('Family/<int:pkF>/Category/', FamilyCategoryList.as_view()),
-    path('Family/<int:pkF>/Category/<pk>', FamilyCategoryDetail.as_view()),
-    path('Family/<int:pkF>/Stockage/', FamilyStockageList.as_view()),
-    path('Family/<int:pkF>/Stockage/<pk>', FamilyStockageDetail.as_view()),
-    path('Family/<int:pkF>/Produit/', FamilyProduitList.as_view()),
-    path('Family/<int:pkF>/Liste/', FamilyListeList.as_view()),
-    # path('Family/<int:pkF>/Produit/<pk>', FamilyProduitDetail.as_view()),
+   path('Family/', FamilyList.as_view()),
+   path('Family/<int:pk>/', FamilyDetail.as_view()),
+   re_path(r'^Family/(?P<code>[A-Z0-9]{10})/$', views.FamilyCode),
+   path('Family/<int:pkF>/Membres/', FamilyMembresList.as_view()),
+   path('Family/<int:pkF>/Repas/', FamilyRepasList.as_view()),
+   path('Family/<int:pkF>/Repas/<pk>', FamilyRepasDetail.as_view()),
+   path('Family/<int:pkF>/Category/', FamilyCategoryList.as_view()),
+   path('Family/<int:pkF>/Category/<pk>', FamilyCategoryDetail.as_view()),
+   path('Family/<int:pkF>/Stockage/', FamilyStockageList.as_view()),
+   path('Family/<int:pkF>/Stockage/<pk>', FamilyStockageDetail.as_view()),
+   path('Family/<int:pkF>/Produit/', FamilyProduitList.as_view()),
+   path('Family/<int:pkF>/Liste/', FamilyListeList.as_view()),
+   #path('Family/<int:pkF>/Produit/<pk>', FamilyProduitDetail.as_view()),
 
-    path('User/', UserList.as_view()),
-    path('User/<pk>', UserDetail.as_view()),
+   path('User/', UserList.as_view()),
+   path('User/<pk>', UserDetail.as_view()),
 
-    path('Repas/', RepasList.as_view()),
-    path('Repas/<pk>/', RepasDetail.as_view()),
+   path('Repas/', RepasList.as_view()),
+   path('Repas/<pk>/', RepasDetail.as_view()),
 
-    path('Category/', CategoryList.as_view()),
-    path('Category/<pk>/', CategoryDetail.as_view()),
+   path('Category/', CategoryList.as_view()),
+   path('Category/<pk>/', CategoryDetail.as_view()),
 
-    path('Stockage/', StockageList.as_view()),
-    path('Stockage/<pk>/', StockageDetail.as_view()),
-    path('Stockage/<pk>/Produit', StockageListProduit.as_view()),
+   path('Stockage/', StockageList.as_view()),
+   path('Stockage/<pk>/', StockageDetail.as_view()),
+   path('Stockage/<pk>/Produit', StockageListProduit.as_view()),
 
-    path('Produit/', ProduitList.as_view()),
-    path('Produit/<pk>/', ProduitDetail.as_view()),
+   path('Produit/', ProduitList.as_view()),
+   path('Produit/<pk>/', ProduitDetail.as_view()),
 
-    path('Liste/', ListeList.as_view()),
-    path('Liste/<pk>/', ListeDetail.as_view()),
-    path('Liste/<pk>/Tache', ListeTacheList.as_view()),
+   path('Liste/', ListeList.as_view()),
+   path('Liste/<pk>/', ListeDetail.as_view()),
+   path('Liste/<pk>/Tache', ListeTacheList.as_view()),
 
-    path('Tache/', TacheList.as_view()),
-    path('Tache/<pk>/', TacheDetail.as_view()),
+   path('Tache/', TacheList.as_view()),
+   path('Tache/<pk>/', TacheDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
